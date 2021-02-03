6802df6a63362d3e090957cc5727acaeb8104aac
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-03 11:59:23 -0600

    scrollspy

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav'
 import Work from './work';
+import Scrollspy from 'react-scrollspy';
 
 import petResume1 from '../assets/imgs/PetResume1.png';
 import petResume2 from '../assets/imgs/PetResume2.png';
@@ -34,38 +34,48 @@ const works = [
 
 export default function Works(props) {
     return (
-        <div style={{
-            height:"100VH",
-            overflowY: "auto",
-            scrollSnapType: "y mandatory"
-        }}>
+        <>
+            <div id="spy-root"
+                style={{
+                    height: "100VH",
+                    overflowY: "auto",
+                    scrollSnapType: "y mandatory",
+                    position: "relative"
+                }}>
 
-            <Nav style={{
-                left: "2em",
-                display:"flex",
-                flexDirection:"column",
-                position: "fixed",
-                height:"100VH",
-                width: "15em",
-                justifyContent: "center",
-                zIndex: "15"
-            }}>
+                <div className="d-flex flex-column justify-content-center h-100">
+                    <Scrollspy items={works.map(work => work.id)}
+                        currentClassName="active"
+                        scrolledPastClassName="past"
+                        rootEl="#spy-root"
+                        style={{
+                            position: 'fixed',
+                            left: "3em",
+                            zIndex: "99",
+                            margin: "auto auto",
+                            listStyle: "none",
+                            width: "15em",
+                            textAlign:"center"
+                        }}
+                    >
+                        {works.map((work, index) => {
+                            return (
+                                <li key={"ni-" + work.id} className="work-nav">
+                                    <a href={"#" + work.id}>{work.name}</a>
+                                </li>
+                            );
+                        })}
+                    </Scrollspy>
+                </div>
+
                 {works.map((work, index) => {
                     return (
-                        <Nav.Item key={"ni-" + work.id}>
-                            <Nav.Link className="work-nav" href={"#" + work.id}>{work.name}</Nav.Link>
-                        </Nav.Item>
+                        <Work key={"c-" + index} id={work.id} images={work.images}>
+                            {work.captions}
+                        </Work>
                     );
                 })}
-            </Nav>
-
-            {works.map((work, index) => {
-                return (
-                    <Work key={"c-" + index} id={work.id} images={work.images}>
-                        {work.captions}
-                    </Work>
-                );
-            })}
-        </div>
+            </div>
+        </>
     );
 }
