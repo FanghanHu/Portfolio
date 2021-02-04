f8c63311a9cfe791a1b5cb881b96395fdd728217
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-03 19:37:03 -0600

    added more work

--- src/pages/works.js
+++ src/pages/works.js
@@ -7,8 +7,58 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import petResume1 from '../assets/imgs/PetResume1.png';
 import petResume2 from '../assets/imgs/PetResume2.png';
 import petResume3 from '../assets/imgs/PetResume3.png';
+import chirper1 from '../assets/imgs/Chirper1.png';
+import chirper2 from '../assets/imgs/Chirper2.png';
+import chirper3 from '../assets/imgs/Chirper3.png';
+import chirper4 from '../assets/imgs/Chirper4.png';
+import chirper5 from '../assets/imgs/Chirper5.png';
+import chirper6 from '../assets/imgs/Chirper6.png';
+import chirper7 from '../assets/imgs/Chirper7.png';
+import chirper8 from '../assets/imgs/Chirper8.png';
+import fo1 from '../assets/imgs/fo76db1.png';
+import fo2 from '../assets/imgs/fo76db2.png';
+import gmp1 from '../assets/imgs/gmp1.png';
+import gmp2 from '../assets/imgs/gmp2.png';
+import gmp3 from '../assets/imgs/gmp3.png';
 
 const works = [
+    {
+        id: "chirper",
+        name: "Chirper POS",
+        images: [chirper1, chirper2, chirper3, chirper4, chirper5, chirper6, chirper7, chirper8],
+        captions: (
+            <>
+                <h3 className="text-white">Chirper POS</h3>
+                <p className="w-100">
+                    A point of sale program for restaurants,
+                    it can help restaurants to keep track of their orders and payments,
+                    manage tables, and record customer information.
+                    Built with: mysql, node.js, express.js, react.
+                    click the visit link to give it a try, the default access code is 0.
+                </p>
+                <div className="m-1 float-left">
+                        <FontAwesomeIcon icon={["fab", 'github']}/>
+                        <a 
+                        href="https://github.com/FanghanHu/Chirper"
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            Source code
+                        </a>
+                    </div>
+                    <div className="m-1 float-right">
+                        <FontAwesomeIcon icon={["fas", 'globe']}/>
+                        <a 
+                        href="https://chirperpos.herokuapp.com/" 
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            visit
+                        </a>
+                    </div>
+            </>
+        )
+    },
     {
         id: "pet-resume",
         name: "Pet Resume",
@@ -45,16 +95,73 @@ const works = [
         )
     },
     {
-        id: "pet-resume2",
-        name: "Pet Resume2",
-        images: [petResume1, petResume2, petResume3],
+        id: "fo76db",
+        name: "Fallout 76 Card Builder",
+        images: [fo1, fo2],
         captions: (
             <>
-                <h3>Pet Resume</h3>
-                <p>asdasdasdasds</p>
+                <h3 className="text-white">Fallout 76 Card Builder</h3>
+                <p className="w-100">
+                    A build planner for the online video game "Fallout 76"
+                    plan your build and share it with friends.
+                    Built with basic html, css, and javascript... oh, also JQuery.
+                </p>
+                <div className="m-1 float-left">
+                        <FontAwesomeIcon icon={["fab", 'github']}/>
+                        <a 
+                        href="https://github.com/FanghanHu/Fallout76CardBuilder"
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            Source code
+                        </a>
+                    </div>
+                    <div className="m-1 float-right">
+                        <FontAwesomeIcon icon={["fas", 'globe']}/>
+                        <a 
+                        href="https://fanghanhu.github.io/Fallout76CardBuilder/index.html" 
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            visit
+                        </a>
+                    </div>
             </>
         )
-    }
+    },
+    {
+        id: "gmp",
+        name: "GMP Website",
+        images: [gmp1, gmp2, gmp3],
+        captions: (
+            <>
+                <h3 className="text-white">GMP Website</h3>
+                <p className="w-100">
+                    Company website for Global Merchant Processing Inc.
+                </p>
+                <div className="m-1 float-left">
+                        <FontAwesomeIcon icon={["fab", 'github']}/>
+                        <a 
+                        href="https://github.com/FanghanHu/GlobalMerchant.us"
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            Source code
+                        </a>
+                    </div>
+                    <div className="m-1 float-right">
+                        <FontAwesomeIcon icon={["fas", 'globe']}/>
+                        <a 
+                        href="http://globalmerchant.us/" 
+                        className="p-1 text-light"
+                        target="_blank" 
+                        rel="noreferrer">
+                            visit
+                        </a>
+                    </div>
+            </>
+        )
+    },
 ];
 
 
