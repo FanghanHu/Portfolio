4ae3d72be922380ea513753dd31f48eab26f073e
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-03 22:10:55 -0600

    starting to work on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 export default function Contact(props) {
     return (
-        <>
-        </>
+        <div className="bg-dark vh-100 vw-100 text-white">
+            <Container>
+                <h1 className="text-center" style={{paddingTop:"7rem"}}>Fanghan Hu</h1>
+            </Container>
+        </div>
     );
 }
