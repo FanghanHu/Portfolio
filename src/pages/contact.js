abb3b3b2aa4535bb942c11297ee385debab61ba6
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-04 19:38:32 -0600

    responsive contact page font size

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,35 +4,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Contact(props) {
     return (
-        <div className="bg-dark vh-100 vw-100 text-white">
+        <div className="bg-dark text-white min-h-100">
             <Container>
-                <h1 className="text-center" style={{ paddingTop: "7rem" }}>Fanghan Hu</h1>
-                <span className="text-muted text-center d-block">Full-Stack Web Developer in Houston, TX</span>
+                <div className="d-flex flex-column vh-100 justify-content-center"  style={{ paddingTop: "7rem" }}>
+                    <h1 className="text-center">Fanghan Hu</h1>
+                    <span className="text-muted text-center d-block">Full-Stack Web Developer in Houston, TX</span>
+                    <span className="text-muted text-center d-block">Status: Freelancing (2/4/2021)</span>
 
-                <div className="d-flex mt-5 mx-auto h3 justify-content-around">
-                    <div>
-                        <FontAwesomeIcon icon={["fab", "github"]} />
-                        <a className="m-3 text-white" href="https://github.com/FanghanHu" target="_blank" rel="noreferrer">Github</a>
+                    <div className="d-flex flex-wrap mt-5 mx-auto contact-text justify-content-around">
+                        <div className="m-2">
+                            <FontAwesomeIcon icon={["fab", "github"]} />
+                            <a className="m-3 text-white" href="https://github.com/FanghanHu" target="_blank" rel="noreferrer">Github</a>
+                        </div>
+
+                        <div className="m-2">
+                            <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                            <a className="m-3 text-white" href="https://www.linkedin.com/in/fanghan-hu/" target="_blank" rel="noreferrer">LinkedIn</a>
+                        </div>
+
+                        <div className="m-2">
+                            <FontAwesomeIcon icon={["fab", "twitter"]} />
+                            <a className="m-3 text-white" href="https://twitter.com/FanghanHu" target="_blank" rel="noreferrer">Twitter</a>
+                        </div>
+
+                        <div className="m-2">
+                            <FontAwesomeIcon icon={["fab", "stack-overflow"]} />
+                            <a className="m-3 text-white" href="https://stackoverflow.com/users/7492159/fanghan-hu" target="_blank" rel="noreferrer">Stack Overflow</a>
+                        </div>
+
+                        <div className="m-2">
+                            <FontAwesomeIcon icon={["fas", "file-pdf"]} />
+                            <a download className="m-3 text-white" href="./Fanghan_Hu.pdf" target="_blank" rel="noreferrer">Resume</a>
+                        </div>
                     </div>
 
-                    <div>
-                        <FontAwesomeIcon icon={["fab", "linkedin"]} />
-                        <a className="m-3 text-white" href="https://www.linkedin.com/in/fanghan-hu/" target="_blank" rel="noreferrer">LinkedIn</a>
-                    </div>
-
-                    <div>
-                        <FontAwesomeIcon icon={["fab", "twitter"]} />
-                        <a className="m-3 text-white" href="https://twitter.com/FanghanHu" target="_blank" rel="noreferrer">Twitter</a>
-                    </div>
-
-                    <div>
-                        <FontAwesomeIcon icon={["fab", "stack-overflow"]} />
-                        <a className="m-3 text-white" href="https://stackoverflow.com/users/7492159/fanghan-hu" target="_blank" rel="noreferrer">Stack Overflow</a>
-                    </div>
-
-                    <div>
-                        <FontAwesomeIcon icon={["fas", "file-pdf"]} />
-                        <a download className="m-3 text-white" href="./Fanghan_Hu.pdf" target="_blank" rel="noreferrer">Resume</a>
+                    <div className="contact-text text-center my-5 text-break">
+                        <div className="my-2">
+                            <FontAwesomeIcon icon={["fas", "mobile-alt"]} />
+                            <span className="mx-3 text-white">(832) 748-8444</span>
+                        </div>
+                        <div className="my-2">
+                            <FontAwesomeIcon icon={["fas", "envelope"]} />
+                            <span className="mx-3 text-white">Fanghan.Hu@Gmail.com</span>
+                        </div>
                     </div>
                 </div>
             </Container>
