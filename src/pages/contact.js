47ef1b7b1ebf6d404238113735970190bcd36d5c
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-02-04 20:46:25 -0600

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,12 +4,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Contact(props) {
     return (
-        <div className="bg-dark text-white min-h-100">
+        <div className="bg-dark text-white min-h-100 overflow-auto">
             <Container>
                 <div className="d-flex flex-column vh-100 justify-content-center"  style={{ paddingTop: "7rem" }}>
-                    <h1 className="text-center">Fanghan Hu</h1>
+                    <h1 className="text-center" 
+                    style={{
+                        fontFamily:'Big Shoulders Display, cursive',
+                        fontSize:"6rem"
+                        }}>Fanghan Hu</h1>
                     <span className="text-muted text-center d-block">Full-Stack Web Developer in Houston, TX</span>
-                    <span className="text-muted text-center d-block">Status: Freelancing (2/4/2021)</span>
+                    <span className="text-muted text-center d-block">Status: Freelancing</span>
+
+                    <div className="mt-5 text-muted h4 text-center" style={{fontFamily:'Big Shoulders Display, cursive'}}>
+                        Hi, I'm Fanghan, I make cross-platform applications, Android applications and responsive websites. I am open to requests, if you need to get in touch, here are some ways you can contact me:
+                    </div>
 
                     <div className="d-flex flex-wrap mt-5 mx-auto contact-text justify-content-around">
                         <div className="m-2">
@@ -41,11 +49,11 @@ export default function Contact(props) {
                     <div className="contact-text text-center my-5 text-break">
                         <div className="my-2">
                             <FontAwesomeIcon icon={["fas", "mobile-alt"]} />
-                            <span className="mx-3 text-white">(832) 748-8444</span>
+                            <a href="tel:832-748-8444" className="mx-3 text-white">(832) 748-8444</a>
                         </div>
                         <div className="my-2">
                             <FontAwesomeIcon icon={["fas", "envelope"]} />
-                            <span className="mx-3 text-white">Fanghan.Hu@Gmail.com</span>
+                            <a  href="mailto:Fanghan.Hu@Gmail.com" className="mx-3 text-white">Fanghan.Hu@Gmail.com</a>
                         </div>
                     </div>
                 </div>
