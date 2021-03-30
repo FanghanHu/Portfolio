6893174999ed07320e915fc5b2604cd47cc612b0
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-03-30 16:12:56 -0500

    align nav

--- src/components/wave-panel/index.js
+++ src/components/wave-panel/index.js
@@ -9,14 +9,14 @@ export default function WavePanel() {
     const [title, setTitle] = useState('Fanghan Hu');
 
     return (
-        <Container>
+        <Container fluid="md">
             <header>
                 <nav className="p-2 d-flex flex-wrap text-white justify-content-between">
-                    <div className="d-flex flex-wrap">
-                        <h5>My Projects:</h5>
+                    <div className="d-flex flex-wrap mb-1">
+                        <div className="h5">My Projects:</div>
                         <div onClick={() => {setTitle("SyrupPOS")}}>SyrupPOS</div>
-                        <div onClick={() => {setTitle("Fallout64 Deck Builder")}}>Fallout64 Deck Builder</div>
                         <div>Pet Resume</div>
+                        <div onClick={() => {setTitle("Fallout64 Deck Builder")}}>Fallout64 Deck Builder</div>
                         <div>GMP Website</div>
                     </div>
                     <div className="d-flex flex-wrap h5">
