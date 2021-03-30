1da016c7d1f197de4a5bd8cd8d6fad20864350f3
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-03-29 21:22:50 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import Works from './pages/works';
-import Contact from "./pages/contact";
-import PageNavbar from './components/pageNavbar';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -15,9 +12,7 @@ function App() {
   return (
     <Container fluid className="p-0">
       <Router basename={process.env.PUBLIC_URL}>
-        <PageNavbar pages={[["/", "Works"],["/contact", "Contact"]]}/>
-        <Route path="/" exact component={Works}/>
-        <Route path="/contact" exact component={Contact}/>
+        
       </Router>
     </Container>
   );
