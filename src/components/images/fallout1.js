2834449edfc7cb2d8996c0dd7e68c3854adde669
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-04-01 10:45:40 -0500

    separate pictures from main file: fallout

---
+++ src/components/images/fallout1.js
@@ -0,0 +1,5 @@
+import image from "../../assets/imgs/fo76db1-min.png";
+
+export default function Fallout1(props) {
+    return <img src={image} alt="temp" {...props}></img>
+}
---
+++ src/components/images/fallout2.js
@@ -0,0 +1,5 @@
+import image from "../../assets/imgs/fo76db2-min.png";
+
+export default function Fallout2(props) {
+    return <img src={image} alt="temp" {...props}></img>
+}
